62823cd1b94fc22cedb1f911be27ee5b1ff87b13
Pétur Runólfsson <peturrun@gmail.com>
2022-08-24 17:27:32 +0000

    Tune evaluation.

--- sample.sh
+++ sample.sh
@@ -3,10 +3,11 @@
 set -e
 
 branch=$(git branch --show-current)
+today=$(date '+%Y%m%d')
 
 c-chess-cli \
-    -engine cmd="./out/seawall.${branch}" name="${branch}" \
-    -engine cmd=./out/seawall.main name="main" \
+    -engine cmd="./out/seawall-${today}-${branch}" \
+    -engine cmd=./out/seawall-${today} \
     -each option.Hash=16 tc=1+0.01 \
     -openings file=./books/training.fen order=random \
     -games 500 -concurrency 2 \

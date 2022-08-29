a97dbcffa823e09be3efdf7c4135c196a76060b0
Pétur Runólfsson <peturrun@gmail.com>
2022-08-29 17:28:50 +0000

    Tune evaluation.

--- sample.sh
+++ sample.sh
@@ -11,4 +11,4 @@ c-chess-cli \
     -each option.Hash=16 tc=1+0.01 \
     -openings file=./books/training.fen order=random \
     -games 500 -concurrency 2 \
-    -sample freq=0.3 decay=0.005 resolve=y file="samples/$(date '+%Y%m%d-%H%M%S').csv" format=csv
+    -sample freq=0.24 decay=0.005 resolve=y file="samples/$(date '+%Y%m%d-%H%M%S').csv" format=csv

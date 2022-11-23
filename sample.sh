fcee5b431d1c87cb69f172773ca485b1fafb0017
Pétur Runólfsson <peturrun@gmail.com>
2022-11-23 07:51:39 +0000

    Tune evaluation.

--- sample.sh
+++ sample.sh
@@ -9,5 +9,5 @@ c-chess-cli \
     -engine cmd="./branches/seawall-main" \
     -each option.Hash=4 tc=1+0.01 \
     -openings file=./books/training.fen order=random \
-    -games 500 -concurrency 2 \
-    -sample freq=0.23 decay=0.005 resolve=y file="samples/$(date '+%Y%m%d-%H%M%S').csv" format=csv
+    -games 600 -concurrency 2 \
+    -sample freq=0.23 decay=0.024 resolve=y file="samples/$(date '+%Y%m%d-%H%M%S').csv" format=csv
